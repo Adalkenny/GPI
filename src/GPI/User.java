87655a8bc22b7ffd1dafe7d09660b2315c1cd9dd
Adalkenny <adalbertmascarenhas1@gmail.com>
2018-12-03 17:28:23 -0100

    Initial commit

---
+++ src/GPI/User.java
@@ -0,0 +1,17 @@
+package GPI;
+
+import javax.swing.*;
+
+public class User {
+    public String dataaaa;
+    public String nomeaaaa;
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
